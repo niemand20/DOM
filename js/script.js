db51156739b668932e58438104dfcfa774961c49
niemand20 <zahid17290@gmail.com>
2022-08-17 11:22:18 +0100

    innerHTML innerText and styles done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-console.log("hello to js");
-console.log(document.body);
+const liCollection = document.getElementsByTagName('li');
+for (const li of liCollection) {
+    // <!-- console.log(li); -->
+    // <!-- console.log(li.innerText); -->
+};
+const allHeadings = document.getElementsByTagName('h1');
+for (const h1 of allHeadings) {
+    // <!-- console.log(h1); -->
+    /* console.log(h1);*/
+    // <!-- console.log(h1.innerText); -->
+}
+
+
+const places = document.getElementsByClassName("important-places");
+console.log(places);
+for (const place of places){
+    console.log(place.innerText);
+};
+
+// querySelectorAll()
+const someLi = document.querySelectorAll(".fruits-container li");
+console.log(someLi);
+for (const li of someLi){
+    console.log(li);
+    console.log(li.innerText);
+}
