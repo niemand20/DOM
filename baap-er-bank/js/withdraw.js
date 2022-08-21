1687c10ad4078ae3c0df41f0bff9892099a25c32
niemand20 <zahid17290@gmail.com>
2022-08-22 00:22:43 +0100

    isNaN and some validation done

--- baap-er-bank/js/withdraw.js
+++ baap-er-bank/js/withdraw.js
@@ -1,27 +1,41 @@
-//1. add event handler with the withdraw button
+///1. add event handler with the withdraw button
 document.getElementById("btn-withdraw").addEventListener("click", function(){
-//2. get the withdrawn amount from the input field
-    const withdrawField = document.getElementById("withdraw-field");
-    const newWithdrawAmount=  parseFloat( withdrawField.value);
+    //2. get the withdrawn amount from the input field
+        const withdrawField = document.getElementById("withdraw-field");
+        const newWithdrawAmount=  parseFloat( withdrawField.value);
+        // 7. withdraw clear
+        withdrawField.value = "";
+        if(isNaN(newWithdrawAmount)){
+            alert("please enter a valid number");
+            return;
+        }
     
-// 3. get previous withdraw total
-    const withdrawTotalElement = document.getElementById("withdraw-total");
-    const previousWithdrawTotal = parseFloat(withdrawTotalElement.innerText);
-    console.log(previousWithdrawTotal);
+    // 3. get previous withdraw total
+        const withdrawTotalElement = document.getElementById("withdraw-total");
+        const previousWithdrawTotal = parseFloat(withdrawTotalElement.innerText);
+        console.log(previousWithdrawTotal);
+    
+    
+    // 5. get the previous Balance total
+        const balanceTotalElement = document.getElementById("balance-total");
+        const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
+        
+        
+        //5.1 if withdraw amount > deposit amount , get an alert
+        if (newWithdrawAmount>previousBalanceTotal){
+            alert("eto teka nai");
+            return;
+        }
 
-// 4.calculate total withdraw amount
-    const currentWithdrawTotal = previousWithdrawTotal+ newWithdrawAmount;
-// 4.5 set total withdraw amount 
-    withdrawTotalElement.innerText = currentWithdrawTotal;
-// 5. get the previous Balance total
-    const balanceTotalElement = document.getElementById("balance-total");
-    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
-    console.log(previousBalanceTotal)
+        // 4.calculate total withdraw amount
+        const currentWithdrawTotal = previousWithdrawTotal+ newWithdrawAmount;
+    // 4.5 set total withdraw amount 
+        withdrawTotalElement.innerText = currentWithdrawTotal;
+    
+    // 6. calculate new balance total and set the new balance total
+        const newBalanceTotal = previousBalanceTotal-newWithdrawAmount;
+        balanceTotalElement.innerText = newBalanceTotal;
+    
+    
+    })
 
-// 6. calculate new balance total and set the new balance total
-    const newBalanceTotal = previousBalanceTotal-newWithdrawAmount;
-    balanceTotalElement.innerText = newBalanceTotal;
-
-// 7. withdraw clear
-    withdrawField.value = "";
-})
