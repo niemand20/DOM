b09715ceb559282b39d1b50d384bc5296d6dd1b8
niemand20 <zahid17290@gmail.com>
2022-08-21 11:59:51 +0100

    navigating between html files and creating header for bank.js

--- baap-er-bank/js/login.js
+++ baap-er-bank/js/login.js
@@ -11,10 +11,10 @@ document.getElementById("btn-submit").addEventListener("click", function(){
 // 4. DANGER: DO NOT VERIFY PASSWORD ON CLIENTSIDE
 // 5. verify email and password
     if (email==="sontan@baap.com" && password==="secret"){
-        console.log("valid user")
+        window.location.href = "bank.html";
     }
     else{
-        console.log("invalid user");
+        alert("wrong password");
     }
 
 })
