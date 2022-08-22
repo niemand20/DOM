1581162c08fafa7949809ddfc731ed28c10d00cb
niemand20 <zahid17290@gmail.com>
2022-08-23 00:52:21 +0100

    price increasing using function

--- shopping-cart-solutions/js/case.js
+++ shopping-cart-solutions/js/case.js
@@ -1,19 +1,33 @@
+function updateCaseNumber(isIncrease){
+     //get the value inside a number field
+     const caseNumberField = document.getElementById("case-number-field");
+     //convert the number to an integer
+     const previousCaseNumber = parseInt(caseNumberField.value);
+     //calculate the new case number
+     let newCaseNumber;
+     if(isIncrease){
+        newCaseNumber = previousCaseNumber+1;
+     }
+     else{
+        newCaseNumber = previousCaseNumber-1;
+     }
+     //set the value to the case number field
+     caseNumberField.value = newCaseNumber;
+     return newCaseNumber;
+     
+
+}
+
+
+
 //add event listener to a case plus button
 document.getElementById("btn-case-plus").addEventListener("click", function(){
-    //get the value inside a number field
-    const caseNumberField = document.getElementById("case-number-field");
-    //convert the number to an integer
-    const previousCaseNumber = parseInt(caseNumberField.value);
-    //calculate the new case number
-    const newCaseNumber =  previousCaseNumber + 1;
-    //set the value to the case number field
-    caseNumberField.value = newCaseNumber
-    
+   const newCaseNumber = updateCaseNumber(true);
+   const caseTotalPrice = newCaseNumber*59;
+   const caseTotalElement = document.getElementById("case-total");
+   caseTotalElement.innerText = caseTotalPrice;
 })
 
 document.getElementById("btn-case-minus").addEventListener("click",function(){
-    const caseNumberMinusField = document.getElementById("case-number-field");
-    const previousCaseNumberForMinusField = parseInt(caseNumberMinusField.value)
-    const newCaseNumberForMinusField =previousCaseNumberForMinusField-1;
-    caseNumberMinusField.value = newCaseNumberForMinusField;
+    updateCaseNumber(false);
 })
