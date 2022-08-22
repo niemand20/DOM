88f25d5fa3dbc8dc2f94345af2d629ea4d9cce52
niemand20 <zahid17290@gmail.com>
2022-08-22 13:38:01 +0100

    27-8 (advanced) Shared Balance update function

--- functional-baap-er-bank/js/withdraw.js
+++ functional-baap-er-bank/js/withdraw.js
@@ -1,1 +1,15 @@
-console.log("withdraw")
+// 1. add withdraw button event handler
+document.getElementById("btn-withdraw").addEventListener("click", function(){
+    //get withdraw amount by using getInputFieldValueById
+    const newWithdrawAmount = getInputFieldValueById("withdraw-field");
+    //get previous withdraw amount by using getTextElementValueById function
+    const previousWithdrawTotal = getTextElementValueById("withdraw-total");
+    //calculate new withdraw total and set the value(setTextElementValueById)
+    const newWithdrawTotal= previousWithdrawTotal + newWithdrawAmount;
+    setTextElementValueById("withdraw-total", newWithdrawTotal);
+    //get previous balance total by using getTextElementValueById function
+    const previousBalanceTotal = getTextElementValueById("balance-total");
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    //set balance total using setTextElementValueById
+    setTextElementValueById("balance-total", newBalanceTotal);
+})
