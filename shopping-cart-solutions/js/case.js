be4e8c902d5c9ca0f592c9e26af8642091f01ef8
niemand20 <zahid17290@gmail.com>
2022-08-23 01:21:50 +0100

    phone price increase and decrease using function

--- shopping-cart-solutions/js/case.js
+++ shopping-cart-solutions/js/case.js
@@ -18,16 +18,23 @@ function updateCaseNumber(isIncrease){
 
 }
 
+function updateCaseTotalPrice(newCaseNumber){
+    
+   const caseTotalPrice = newCaseNumber*59;
+   const caseTotalElement = document.getElementById("case-total");
+   caseTotalElement.innerText = caseTotalPrice;
+
+}
 
 
 //add event listener to a case plus button
 document.getElementById("btn-case-plus").addEventListener("click", function(){
-   const newCaseNumber = updateCaseNumber(true);
-   const caseTotalPrice = newCaseNumber*59;
-   const caseTotalElement = document.getElementById("case-total");
-   caseTotalElement.innerText = caseTotalPrice;
+   
+    const newCaseNumber = updateCaseNumber(true);
+    updateCaseTotalPrice(newCaseNumber);
 })
 
 document.getElementById("btn-case-minus").addEventListener("click",function(){
-    updateCaseNumber(false);
+    const newCaseNumber =  updateCaseNumber(false);
+    updateCaseTotalPrice(newCaseNumber);
 })
